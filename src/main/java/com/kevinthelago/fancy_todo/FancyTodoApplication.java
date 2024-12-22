b1b11828f540d3c1c904246612acef7471321b0f
Kevin Lago <kevinthelago@gmail.com>
2024-12-22 15:35:02 -0500

    Initial Commit

---
+++ src/main/java/com/kevinthelago/fancy_todo/FancyTodoApplication.java
@@ -0,0 +1,13 @@
+package com.kevinthelago.fancy_todo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FancyTodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FancyTodoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kevinthelago/fancy_todo/FancyTodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kevinthelago.fancy_todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FancyTodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
